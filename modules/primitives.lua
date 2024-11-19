05414180c9ead3e2de42f73ebcebd18fa6d9a3b7
Luxen De'Mark <Luxen@gmx.com>
2024-11-19 10:58:34 -0500

    added hslider/vslider, frames default expand to NO

--- modules/primitives.lua
+++ modules/primitives.lua
@@ -13,14 +13,15 @@ local he = {} -->>public.primitives._func()
 
 --transparent frame template
 he.clearframe = function(intable)
-	assert(type(intable) == "table", "Helium.clearframe expects a table for its argument, got a " .. type(intable))
-	assert((iup.IsValid(intable[1])) or (intable[1] == nil), "Helium.solidframe input table did not have a valid IUP element at [1]; got " .. type(intable[1]))
+	--assert(type(intable) == "table", "Helium.clearframe expects a table for its argument, got a " .. type(intable))
+	--assert((iup.IsValid(intable[1])) or (intable[1] == nil), "Helium.solidframe input table did not have a valid IUP element at [1]; got " .. type(intable[1]))
 	
 	local defaults = {
 		image = private.tryfile("solidframe.png"),
 		bgcolor = "0 0 0 0 *",
 		segmented = "0 0 1 1",
 		iup.vbox { },
+		expand = "NO",
 	}
 	
 	for k, v in pairs(intable) do
@@ -42,6 +43,7 @@ he.solidframe = function(intable)
 		bgcolor = "255 255 255 255 *",
 		segmented = "0.1 0.1 0.9 0.9",
 		iup.vbox { },
+		expand = "NO",
 	}
 	
 	for k, v in pairs(intable) do
@@ -62,6 +64,7 @@ he.borderframe = function(intable)
 		bgcolor = "255 255 255 255 *",
 		segmented = "0.1 0.1 0.9 0.9",
 		iup.vbox { },
+		expand = "NO",
 	}
 	
 	for k, v in pairs(intable) do
@@ -146,6 +149,161 @@ he.progressbar = function(intable)
 	end
 	
 	return iup.progressbar(defaults)
+end
+
+
+
+he.hslider = function(intable)
+	
+	local scroll_timer = Timer()
+		--scroll_cb is called EVERY FRAME during a drag event. if your function is heavy, use scroll_event_cb() instead. There should be an EVEN better way of doing this, but i've not found it
+	local scroll_flag = false
+	
+	local default = {
+		xmin = 0,
+		xmax = 100,
+		dx = 30,
+		posx = 0,
+		scrollbar = "HORIZONTAL",
+		expand = "HORIZONTAL",
+		scroll_event_cb = function() end, --called by timer when flagged with default scroll_cb()
+		scroll_cb = function(self)
+			scroll_flag = true
+		end,
+		border = "NO",
+	}
+	
+	for k, v in pairs(intable) do
+		default[k] = v
+	end
+	
+	local scroller = iup.canvas(default)
+	
+	scroller.get_pos = function(self)
+		--get percentage position
+		return (self.posx / (self.xmax - self.xmin) * 100)
+	end
+	
+	local scroll_update
+	scroll_update = function()
+		if not iup.IsValid(scroller) then
+			scroll_timer:Kill()
+			return
+		end
+		
+		if scroll_flag then
+			default.scroll_event_cb(scroller)
+			scroll_flag = false
+		end
+		
+		scroll_timer:SetTimeout(1, scroll_update)
+	end
+	
+	scroller.init_timer = function(self)
+		scroll_update()
+	end
+	
+	scroller.map_cb = function(self)
+		--if map_cb is replaced, this needs to be added manually to it
+		self:init_timer()
+	end
+	
+	return scroller
+end
+
+
+--[[
+	old vslider element, kept in case I need it for some stupid reason
+he.vslider = function(intable)
+	
+	local scroll_timer = Timer()
+		--scroll_cb is called EVERY FRAME during a drag event. if your function is heavy, use scroll_event_cb() instead. There should be an EVEN better way of doing this, but i've not found it
+	
+	local default = {
+		ymin = 0,
+		ymax = 100,
+		dy = 30,
+		posy = 0,
+		scrollbar = "VERTICAL",
+		expand = "VERTICAL",
+		scroll_event_cb = function() end, --called by timer with default scroll_cb()
+		scroll_cb = function(self)
+			scroll_timer:SetTimeout(1, self.scroll_event_cb)
+		end,
+		border = "NO",
+	}
+	
+	for k, v in pairs(intable) do
+		default[k] = v
+	end
+	
+	local scroller = iup.canvas(default)
+	
+	scroller.get_pos = function(self)
+		--get percentage position
+		return (self.posy / (self.ymax - self.ymin) * 100)
+	end
+	
+	return scroller
+end
+]]--
+
+he.vslider = function(intable)
+	
+	local scroll_timer = Timer()
+		--scroll_cb is called EVERY FRAME during a drag event. if your function is heavy, use scroll_event_cb() instead. There should be an EVEN better way of doing this, but i've not found it
+	local scroll_flag = false
+	
+	local default = {
+		ymin = 0,
+		ymax = 100,
+		dy = 30,
+		posy = 0,
+		scrollbar = "VERTICAL",
+		expand = "VERTICAL",
+		scroll_event_cb = function() end, --called by timer when flagged with default scroll_cb()
+		scroll_cb = function(self)
+			scroll_flag = true
+		end,
+		border = "NO",
+	}
+	
+	for k, v in pairs(intable) do
+		default[k] = v
+	end
+	
+	local scroller = iup.canvas(default)
+	
+	scroller.get_pos = function(self)
+		--get percentage position
+		return (self.posy / (self.ymax - self.ymin) * 100)
+	end
+	
+	local scroll_update
+	scroll_update = function()
+		if not iup.IsValid(scroller) then
+			scroll_timer:Kill()
+			return
+		end
+		
+		if scroll_flag then
+			default.scroll_event_cb(scroller)
+			scroll_flag = false
+		end
+		
+		scroll_timer:SetTimeout(1, scroll_update)
+	end
+	
+	scroller.init_timer = function(self)
+		scroll_update()
+	end
+	
+	scroller.map_cb = function(self)
+		--if map_cb is replaced, this needs to be added manually to it
+		self:init_timer()
+	end
+	
+	return scroller
 end
 
 
