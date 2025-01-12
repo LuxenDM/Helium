b8ea255bc10317ea301b7da9f558884b6dadb9b9
Luxen De'Mark <81200411+LuxenDM@users.noreply.github.com>
2025-01-12 16:29:17 -0500

    Created stubs for preset generators

--- modules/gui_presets.lua
+++ modules/gui_presets.lua
@@ -7,16 +7,42 @@ local he = {} --these go to presets
 
 
 
---button to launch a dragable item.
-he.drag_item = function()
-	
+--subdialog: modal-style popup with custom placement, with screen-edge awareness
+he.subdialog = function(intable)
+	local default = {
+		lock_focus = "NO",
+		alignment = "ACENTER",
+		pos_x = nil,
+		pos_y = nil, --replaced with screen center coords if nil
+		[1] = iup.vbox { },
+		--size = resolve(THIRDxTHIRD),
+	}
 end
 
+--context_menu: subdialog to mimic single-layer context menu behavior
+he.context_menu = function(intable)
+	local default = {
+		pos_x = nil,
+		pos_y = nil, --defaults to user mouse position
+		button_provider = iup.stationbutton,
+		[1] = "Menu",
+	}
+end
 
+--alert: preset subdialog to display a message to the user.
+he.alert = function(intable)
+	local default = {
+		[1] = "alert message",
+		timeout = -1,
+		response = "Okay",
+	}
+end
 
---panel to accept dragged items
-he.drag_target = function()
-	
+--reader: preset subdialog to display a large text block.
+he.reader = function(intable)
+	local default = {
+		[1] = "Large text block",
+	}
 end
 
 
